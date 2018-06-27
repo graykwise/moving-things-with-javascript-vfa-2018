59a073efe4ffcf38010f004b9e3a6c9dacfdf80b
Grayson Wise <graysonwise3@gmail.com>
2018-06-27 03:57:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
 const app = "I don't do much."
-
-var dodger = document.getElementById('dodger')
-
-dodger.style.backgroundColor = "#FF69B4"
-
-dodger.style.bottom = '100px'
-
-document.addEventListener('keydown', function(e) {
-  if (e.which === 37) {
-    moveDodgerLeft()
-  }
-})
